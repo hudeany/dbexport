d6d073cc79125b9f01de11b82bc4dbcd01e63fde
aso <aso@VPN-Notebook>
2016-01-10 22:24:51 +0100

    BugFix of Update

--- src/de/soderer/dbcsvexport/DbCsvExport.java
+++ src/de/soderer/dbcsvexport/DbCsvExport.java
@@ -16,9 +16,9 @@ import de.soderer.utilities.WorkerParentDual;
 import de.soderer.utilities.WorkerSimple;
 
 public class DbCsvExport implements WorkerParentDual, UpdateParent {
-	public static final String VERSION = "3.1.0";
+	public static final String VERSION = "3.2.1";
 	public static final String APPLICATION_NAME = "DbCsvExport";
-	public static final String VERSIONINFO_DOWNLOAD_URL = "http://www.soderer.de/index.php?download=Versions.xml";
+	public static final String VERSIONINFO_DOWNLOAD_URL = "http://downloads.sourceforge.net/project/dbcsvexport/Versions.xml?r=&ts=<time_seconds>&use_mirror=master";
 	public static final File CONFIGURATION_FILE = new File(System.getProperty("user.home") + File.separator + ".DbCsvExport.config");
 
 	private static String USAGE_MESSAGE = "DbCsvExport (by Andreas Soderer, mail: dbcsvexport@soderer.de)\n"
