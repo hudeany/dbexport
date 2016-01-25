5a74fc14a69bbc8386c8d9e8a2ecb649b8060d43
aso <aso@VPN-Notebook>
2016-01-25 09:53:32 +0100

    BugFix in IndetedXmlWriter

--- src/de/soderer/dbcsvexport/DbCsvExport.java
+++ src/de/soderer/dbcsvexport/DbCsvExport.java
@@ -17,7 +17,7 @@ import de.soderer.utilities.WorkerDual;
 import de.soderer.utilities.WorkerParentDual;
 
 public class DbCsvExport extends BasicUpdateableConsoleApplication implements WorkerParentDual {
-	public static final String VERSION = "3.9.0";
+	public static final String VERSION = "3.9.1";
 	public static final String APPLICATION_NAME = "DbCsvExport";
 	public static final String VERSIONINFO_DOWNLOAD_URL = "http://downloads.sourceforge.net/project/dbcsvexport/Versions.xml?r=&ts=<time_seconds>&use_mirror=master";
 	public static final File CONFIGURATION_FILE = new File(System.getProperty("user.home") + File.separator + ".DbCsvExport.config");
